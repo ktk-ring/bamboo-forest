7149219af6656d2f5f1a299f6fff4ed362120947
Tae-kyun Kim <ktk03115@gmail.com>
2022-12-15 01:15:52 +0900

    Create bamboo-forest.ino

---
+++ bamboo-forest/bamboo-forest.ino
@@ -0,0 +1,21 @@
+#include <RF24.h>
+#include <RF24_config.h>
+#include <nRF24L01.h>
+#include <printf.h>
+
+#include <MPU6050.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <MPU6050_9Axis_MotionApps41.h>
+#include <helper_3dmath.h>
+
+#include <I2Cdev.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
